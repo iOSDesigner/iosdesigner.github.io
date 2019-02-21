d0e81c1ea6123f9e7aa686af03dba7c57afc62ca
iOSDesigner <andereder@yahoo.de>
2019-02-21 17:59:04 +0100

    Theme

--- repo/packages.sh
+++ repo/packages.sh
@@ -3,6 +3,6 @@ dpkg-deb -bZgzip projects/liberte
 dpkg-deb -bZgzip projects/iosmojave
 dpkg-deb -bZgzip projects/biscuitios11
 dpkg-deb -bZgzip projects/minimalist
-dpkg-deb -bZgzip projects/vivido
+dpkg-deb -bZgzip projects/vivido1
 dpkg-deb -bZgzip projects/vivido2
 dpkg-deb -bZgzip projects/vivido3
