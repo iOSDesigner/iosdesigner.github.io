a99327ec783e5834638c1ddd5c151de7969da4a4
iOSDesigner <applemarceltech@gmail.com>
2020-01-01 18:13:40 +0100

    Update

--- iosdesigner.github.io-master/repo/packIt.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-bzip2 -z ./Packages
--- iosdesigner.github.io-master/repo/packages.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-dpkg-deb -bZgzip projects/Minimalios12
-dpkg-deb -bZgzip projects/biscuitios11
-dpkg-deb -bZgzip projects/minimalios11
-dpkg-deb -bZgzip projects/nini
-dpkg-deb -bZgzip projects/Niki
-dpkg-deb -bZgzip projects/Rainbow
