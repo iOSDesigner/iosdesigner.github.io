80ca772a0d5c3f9b2051ee25624b43df2e419155
iOSDesigner <applemarceltech@gmail.com>
2020-01-13 20:10:14 +0100

    theme

--- repo/packages.sh
+++ repo/packages.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
-dpkg-deb -bZgzip projects/Minimalios12
-dpkg-deb -bZgzip projects/biscuitios11
+dpkg-deb -bZgzip projects/AureliaOS
+dpkg-deb -bZgzip projects/Libertev2
 dpkg-deb -bZgzip projects/minimalios11
-dpkg-deb -bZgzip projects/nini
-dpkg-deb -bZgzip projects/Niki
-dpkg-deb -bZgzip projects/Rainbow
+dpkg-deb -bZgzip projects/Rainbowv2
